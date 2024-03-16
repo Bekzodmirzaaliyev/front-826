00024062d32119ec87a380b277d39a142500462a
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-03-16 17:06:07 +0500

    ASD

---
+++ src/components/Sets/Sets.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { GrShop } from "react-icons/gr";
+
+const Sets = () => {
+  const [set, setSet] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3005/popular-sets")
+      .then((data) => data.json())
+      .then((res) => {
+        console.log("sets: ", res);
+        setSet(res);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold pb-4 text-center border-t border-gray-300">
+        <span className="bg-white px-3">Популярные наборы</span>
+      </h2>
+      <div className="flex flex-wrap justify-center">
+        {set.map((item, index) => (
+          <div
+            key={index}
+            className="w-full h-full max-w-[370px] max-h-[460px] mx-2 my-4 bg-white rounded-lg shadow-md border border-gray-300"
+          >
+            <div className="product-img">
+              <img
+                src={item.img}
+                alt=""
+                className=" w-full h-[60%] max-h-[300px] rounded-md shadow-md "
+              />
+            </div>
+
+            <div className=" p-3">
+              <p className="text-lg font-semibold mb-2">{item.name}</p>
+              <p className="text-[#292929] font-normal text-sm mb-2">
+                {item.description}
+              </p>
+              <div className="price flex items-center justify-between mt-4 border-t border-gray-300 pt-4">
+                <p className="text-[#E7426A] font-semibold text-lg border-r border-gray-300 pr-40">
+                  {item.price} руб
+                </p>
+                <div className="tobasket flex items-center">
+                  <GrShop className="mr-1" />
+                  <p className="font-semibold text-sm hover:text-[#E7426A]">
+                    В корзину
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="sets-btn flex justify-center mt-4">
+        <button className="py-5 px-12 border border-[#4E9DD3] font-semibold rounded text-base hover:bg-[#4E9DD3] hover:text-white transition-all">
+          Все праздничные наборы
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sets;
---
+++ src/components/Swiper/Swiper.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+
+export default function SwiperHeader() {
+  return (
+    <div className="w-full flex justify-center">
+      <Swiper
+        className="w-full"
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+      >
+        <SwiperSlide className="w-full">
+          <div className="swiper-slide bg-[#D4E9F9] h-[50px] flex items-center justify-center gap-12">
+            <div className="slide-logo relative">
+              <img
+                className="w-[32px] h-[31px]"
+                src="src/components/Swiper/images/Group 108.png"
+                alt=""
+              />
+              <img
+                className="absolute bottom-0 left-3 w-[45px] h-[17px]"
+                src="src/components/Swiper/images/Group 110.png"
+                alt=""
+              />
+            </div>
+            <p className="text-lg font-normal">
+              МИНДАЛЬНАЯ МУКА И НАТУРАЛЬНЫЕ ИНГРЕДИЕНТЫ
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="w-full">
+          <div className="swiper-slide bg-[#D4E9F9] h-[50px] flex items-center justify-center gap-5">
+            <div className="slide-logo relative">
+              <img
+                className="w-[41px] h-[40px]"
+                src="src/components/Swiper/images/Group 216.png"
+                alt=""
+              />
+            </div>
+            <p className="text-lg font-normal">ВСЕГДА СВЕЖЕЕ</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="w-full">
+          <div className="swiper-slide bg-[#D4E9F9] h-[50px] flex items-center justify-center gap-7">
+            <div className="slide-logo relative">
+              <img
+                className="w-[45px] h-[35px]"
+                src="src/components/Swiper/images/Group 214.png"
+                alt=""
+              />
+            </div>
+            <p className="text-lg font-normal">
+              ОПТОВЫЕ ПОСТАВКИ ОТ ПРОИЗВОДИТЕЛЯ
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="w-full">
+          <div className="swiper-slide bg-[#D4E9F9] h-[50px] flex items-center justify-center gap-7">
+            <div className="slide-logo relative">
+              <img
+                className="w-[45px] h-[31px]"
+                src="src/components/Swiper/images/Group 213.png"
+                alt=""
+              />
+            </div>
+            <p className="text-lg font-normal">ДОСТАВКА В ДЕНЬ ЗАКАЗА</p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
