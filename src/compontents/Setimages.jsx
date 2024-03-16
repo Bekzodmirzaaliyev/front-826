936daf9811396c33cca204dd72957c0d7e88761c
shakhriyor23 <sajlavovsahrier@gmail.com>
2024-03-16 18:03:42 +0500

    ASD

--- src/compontents/Setimages.jsx
+++ src/compontents/Setimages.jsx
@@ -9,7 +9,7 @@ const Setimages = () => {
       </div>
       <div className='text-center flex items-center justify-center flex-col flex-1 gap-5'>
         <img className='' src="./src/imaegs/Group 212.png" alt="" />
-        <h2 className='font-montserrat font-semibold text-center text-4xl leading-10 tracking-normal  '>Настоящая любовь</h2>
+        <h2 className='font-montserrat font-semibold text-center text-4xl leading-10 tracking-normal'>Настоящая любовь</h2>
         <p className='font-proxima font-normal text-center text-lg leading-6 tracking-normal'>Пирожные макарон и другие десерты <br /> c из натуральных ингредиентов, приготовленные с любовью</p>
       </div>
     </div>
