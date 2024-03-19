b163309053a9e82e8a60bc5e799f0fcdca2d7470
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-03-19 18:09:38 +0500

    ADS

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Sets from "../components/Sets/Sets";
 import News from "../components/News/News";
+import { SwiperSlide } from "swiper/react";
+import SwiperHeader from "../components/Swiper/Swiper";
 
 const Header = () => {
   return (
     <div>
+      <SwiperHeader />
       <Navbar />
       <Sets/>
       <News/>
