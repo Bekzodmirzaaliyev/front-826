485c708c4c51aeb8ea0a02f0aad92c81329314af
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-03-19 17:56:17 +0500

    KOomol

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import Header from '../layout/Header';
 import Main from '../layout/Main';
-import Navbar from "../components/Navbar/Navbar";
-import SwiperHeader from "../components/Swiper/Swiper";
-import Sets from "../components/Sets/Sets";
-import Cared from '../components/Cared/Cared';
+
 
 const Home = () => {
   return <div>
